73ac1ed1b9618652ac95fa4b264e5cd425a4f81b
Alireza Rahimi <azerila406@gmail.com>
2024-12-30 10:56:34 +0330

    add whichRoomID

--- include/game.h
+++ include/game.h
@@ -77,6 +77,7 @@ void renderHUD(Level*);
 
 /* Tile Fucntions*/
 char tileChar(Tile*);
+int whichRoomID(Level*, int, int);
 
 /* Player Functions*/
 void initPlayer(Player*, Level*, int);  // AFTER LEVEL INIT
--- src/tile.c
+++ src/tile.c
@@ -1,2 +1,10 @@
 #include "game.h"
 
+int whichRoomID(Level* L, int x, int y) {
+  for (int i = 0; i < L->num_room; ++i) {
+    int x0 = L->room[i].x, x1 = L->room[i].x + L->room[i].height;
+    int y0 = L->room[i].y, y1 = L->room[i].y + L->room[i].width;
+    if (x0 <= x && x <= x1 && y0 <= y && y <= y1) return i;
+  }
+  return -1;
+}
