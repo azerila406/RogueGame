514670fb4c80425973068a8370034017c90def39
Alireza Rahimi <azerila406@gmail.com>
2025-01-05 13:47:17 +0330

    add multi-level map

--- src/game.c
+++ src/game.c
@@ -8,7 +8,20 @@ void initGame() {
   G = (Game *)malloc(sizeof(Game));
   G->cur = 0;
   initLevel(&(G->lvl[0]));
-  initPlayer(P, &(G->lvl[G->cur]), DIFF_LEVELS[DIFF_LEVEL]);
+  initPlayer(P, &(G->lvl[0]), DIFF_LEVELS[DIFF_LEVEL]);
+  for (int i = 0; i + 1 < MAX_LEVELS; ++i) {
+    int r, x, y;
+    do {
+      r = rand() % G->lvl[i].num_room;
+      x = rnd(getX0(&(G->lvl[i].room[r])) + 1, getX1(&(G->lvl[i].room[r])) - 1);
+      y = rnd(getY0(&(G->lvl[i].room[r])) + 1, getY1(&(G->lvl[i].room[r])) - 1);
+    } while (G->lvl[i].tile[x][y].type);
+    G->lvl[i].tile[x][y].type = 8;
+    do {
+      initLevelWithARoom(&(G->lvl[i + 1]), &(G->lvl[i].room[r]));
+    } while (G->lvl[i + 1].tile[x][y].type);
+    G->lvl[i + 1].tile[x][y].type = 10;
+  }
 }
 
 void processInput(int x, Level *L) {
